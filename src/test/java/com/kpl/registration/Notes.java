2b6a6cc5b13deea42366297191c6119b6a55464f
Anupam Podder <100278900+anupam101999@users.noreply.github.com>
2024-07-08 21:06:12 +0530

    lombok add notes

---
+++ src/test/java/com/kpl/registration/Notes.java
@@ -0,0 +1,3 @@
+// eclipse lombok addition 
+// go to eclipse configuration settings just below eclipse app then set path as --
+// -javaagent:C:\Anupam Repo\eclipse-java-2023-06-R-win32-x86_64\eclipse\lombok.jar
