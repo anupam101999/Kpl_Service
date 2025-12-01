6a8cb195d23c1b25d2782181fdb339015f95d96f
Anupam <testuser@gmail.com>
2025-12-01 17:15:23 +0530

    New Code Added #1

---
+++ src/main/java/com/kpl/registration/util/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.kpl.registration.util;
+
+
+public class CommonConstants {
+    public static String STUDENT_CREATED = "Student Created Successfully";
+    public static String STUDENT_EXIST = "Student Exist With Same Name";
+}
